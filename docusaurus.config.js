0afbb61deeea1541bc953248523838999f47c785
PaulieB14 <94752445+PaulieB14@users.noreply.github.com>
2022-09-16 22:58:59 -0400

    first commit

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -8,7 +8,7 @@ const darkCodeTheme = require('prism-react-renderer/themes/dracula');
 const config = {
   title: 'HedgePlus',
   tagline: 'Dinosaurs are cool',
-  url: 'https://www.hedgeplus.io/docs',
+  url: 'https://hedgeplus-io/docs',
   baseUrl: '/',
   onBrokenLinks: 'throw',
   onBrokenMarkdownLinks: 'warn',
@@ -17,7 +17,7 @@ const config = {
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
   organizationName: 'paulieb14', // Usually your GitHub org/user name.
-  projectName: 'hedgeplus-docs', // Usually your repo name.
+  projectName: 'hedgeplus-io-docs', // Usually your repo name.
 
   // Even if you don't use internalization, you can use this field to set useful
   // metadata like html lang. For example, if your site is Chinese, you may want
