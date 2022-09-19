1e25b7b3512363c077bf9812d70228d988daca19
PaulieB14 <94752445+PaulieB14@users.noreply.github.com>
2022-09-19 09:08:30 -0400

    Update docusaurus.config.js

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -8,7 +8,7 @@ const darkCodeTheme = require('prism-react-renderer/themes/dracula');
 const config = {
   title: 'HedgePlus',
   tagline: 'Dinosaurs are cool',
-  url: 'https://hedgeplus-io/docs',
+  url: 'https://paulieb14@github.io',
   baseUrl: '/',
   onBrokenLinks: 'throw',
   onBrokenMarkdownLinks: 'warn',
