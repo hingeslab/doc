080ac65139ee1756b297567994f21b4b5c0674ad
PaulieB14 <94752445+PaulieB14@users.noreply.github.com>
2023-03-16 17:10:35 -0400

    Update docusaurus.config.js
    changed url

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -8,7 +8,7 @@ const darkCodeTheme = require('prism-react-renderer/themes/dracula');
 const config = {
   title: 'Hinges',
   tagline: 'Dinosaurs are cool',
-  url: 'process.env.BASE_URL',
+  url: 'https://mydefaulturl.asafallback.example',
   baseUrl: '/',
   onBrokenLinks: 'warn',
   onBrokenMarkdownLinks: 'warn',
