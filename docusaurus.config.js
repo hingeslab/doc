ced534eccb8d3d32321f4e500b3d4c40dacd9f30
PaulieB14 <94752445+PaulieB14@users.noreply.github.com>
2023-03-16 19:24:27 -0400

    Update docusaurus.config.js
    URL

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -8,7 +8,7 @@ const darkCodeTheme = require('prism-react-renderer/themes/dracula');
 const config = {
   title: 'Hinges',
   tagline: 'Dinosaurs are cool',
-  url: 'https://mydefaulturl.asafallback.example',
+  url: 'https://hinges.vercel.app/',
   baseUrl: '/',
   onBrokenLinks: 'warn',
   onBrokenMarkdownLinks: 'warn',
