c505b0ef2a379087ccda5acbd497170f7b0b1b7c
PaulieB14 <94752445+PaulieB14@users.noreply.github.com>
2023-03-15 21:54:51 -0400

    URL FIx

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -8,8 +8,8 @@ const darkCodeTheme = require('prism-react-renderer/themes/dracula');
 const config = {
   title: 'Hinges',
   tagline: 'Dinosaurs are cool',
-  url: 'https://paulieb14@github.io/',
-  baseUrl: '/github/',
+  url: 'docs.hinges.io/',
+  baseUrl: '/hinges/',
   onBrokenLinks: 'warn',
   onBrokenMarkdownLinks: 'warn',
   favicon: 'img/favicon.ico',
