e50770fbeae871640a04614b03555a28dcca7b87
PaulieB14 <94752445+PaulieB14@users.noreply.github.com>
2023-03-16 22:09:52 -0400

    Update docusaurus.config.js
    set to not collapse

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -145,4 +145,16 @@ const config = {
     }),
 };
 
-module.exports = config;
+module.exports = {
+  docs: {
+    Guides: [
+      'creating-pages',
+      {
+        type: 'category',
+        label: 'Docs',
+        collapsed: false,
+        items: ['markdown-features', 'sidebar', 'versioning'],
+      },
+    ],
+  },
+};
