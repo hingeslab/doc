a480afbb03d30b0aaaa81ec12dc02382664fa633
ninjadev0706 <ninjadeveloper0706@gmail.com>
2023-03-23 07:59:57 +0900

    changed collapse

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -36,7 +36,7 @@ const config = {
         docs: {
           showLastUpdateTime: false,
           routeBasePath: '/',
-          sidebarCollapsed: false,
+          sidebarCollapsed: true,
           sidebarPath: require.resolve('./sidebars.js'),
           // Please change this to your repo.
           // Remove this to remove the "edit this page" links.
