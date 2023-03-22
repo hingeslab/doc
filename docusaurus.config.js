a687c8b339cbb425c07b054f78ab3ff617349119
PaulieB14 <94752445+PaulieB14@users.noreply.github.com>
2023-03-22 11:42:02 -0400

    Update docusaurus.config.js
    revised edit link

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -41,7 +41,7 @@ const config = {
           // Please change this to your repo.
           // Remove this to remove the "edit this page" links.
           editUrl:
-            'https://github.com/hingeslab/doc/',
+            'https://github.com/hingeslab/docusaurus.community/edit/main/',  
         },
         blog:false,
         //blog: {
